e9ef3b044a034e9f76ddc6b3e75d46afe3346389
zira777ru@gmail.com <zira777ru@gmail.com>
2023-06-14 15:52:39 -0700

    v0.1

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+# Dockerfile
+FROM python:3.10
+
+COPY requirements.txt .
+RUN pip install --upgrade pip
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 7777
+
+CMD ["uvicorn", "main:app", "--proxy-headers", "--host", "0.0.0.0", "--port", "7777"]
---
+++ app.py
@@ -0,0 +1,59 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import requests
+from pydantic import BaseModel
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+class LocationData(BaseModel):
+    latitude: float
+    longitude: float
+
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/location")
+async def save_location(request: Request, location_data: LocationData):
+    latitude = location_data.latitude
+    longitude = location_data.longitude
+
+    # Получение информации о местоположении по координатам
+    response = requests.get(f"https://geocode-maps.yandex.ru/1.x/?format=json&geocode={longitude},{latitude}&apikey=a9009ca1-bc6d-4acb-b3e5-a970381522b0")
+    location_data = response.json()
+
+    # Обработка данных о местоположении
+    if 'response' in location_data and 'GeoObjectCollection' in location_data['response']:
+        geo_objects = location_data['response']['GeoObjectCollection']['featureMember']
+        if len(geo_objects) > 0:
+            geo_object = geo_objects[0]['GeoObject']
+            address = geo_object['metaDataProperty']['GeocoderMetaData']['text']
+            components = geo_object['metaDataProperty']['GeocoderMetaData']['Address']['Components']
+
+            # Поиск города и страны в компонентах адреса
+            city = ''
+            country = ''
+            for component in components:
+                if 'locality' in component['kind']:
+                    city = component['name']
+                elif 'country' in component['kind']:
+                    country = component['name']
+
+            # Подготовка данных для отправки на клиент
+            result = {
+                'latitude': latitude,
+                'longitude': longitude,
+                'city': city,
+                'country': country,
+                'address': address
+            }
+            return result
+
+    # Если не удалось получить данные о местоположении
+    return {'error': 'Location data not found'}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, port=8000)
